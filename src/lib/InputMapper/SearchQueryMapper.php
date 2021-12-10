88b730a4bee29c7a1319491eb193889508443576
konradoboza <konrad.oboza@ez.no>
2021-12-10 16:05:30 +0100

    IBX-1604: Fixed deprecation warning related to PHP8

--- src/lib/InputMapper/SearchQueryMapper.php
+++ src/lib/InputMapper/SearchQueryMapper.php
@@ -129,12 +129,8 @@ final class SearchQueryMapper implements QueryMapper
      *
      * @return \Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion\DateMetadata[]
      */
-    private function mapDateMetadata(array $queryArg = [], $dateMetadata)
+    private function mapDateMetadata(array $queryArg, $dateMetadata)
     {
-        if (!isset($queryArg[$dateMetadata]) || !is_array($queryArg[$dateMetadata])) {
-            return [];
-        }
-
         $targetMap = [
             'Created' => Query\Criterion\DateMetadata::CREATED,
             'Modified' => Query\Criterion\DateMetadata::MODIFIED,
