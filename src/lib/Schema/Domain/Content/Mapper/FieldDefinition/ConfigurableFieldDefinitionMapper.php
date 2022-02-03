580df9d39727be4f7448123a40deb3d230448a86
Slawomir Dolzycki-Uchto <Nattfarinn@users.noreply.github.com>
2022-02-03 12:12:39 +0100

    Update src/lib/Schema/Domain/Content/Mapper/FieldDefinition/ConfigurableFieldDefinitionMapper.php
    Co-authored-by: Dawid Parafiński <dawid.parafinski@ez.no>

--- src/lib/Schema/Domain/Content/Mapper/FieldDefinition/ConfigurableFieldDefinitionMapper.php
+++ src/lib/Schema/Domain/Content/Mapper/FieldDefinition/ConfigurableFieldDefinitionMapper.php
@@ -14,7 +14,7 @@ class ConfigurableFieldDefinitionMapper implements FieldDefinitionMapper, FieldD
 {
     /**
      * @var array
-     * Map ofibexa field type identifier to a GraphQL type
+     * Map of ibexa field type identifier to a GraphQL type
      */
     private $typesMap;
 
