ba5e369cb913fce42cf47cdc87445b6dca895a64
Konrad Oboza <konrad.oboza@ibexa.co>
2024-07-01 12:58:56 +0200

    IBX-8356: Reworked `Ibexa\Core\MVC\Symfony\Security\Authentication\AuthenticatorInterface` usages to comply with Symfony-based authentication (#67)
    * IBX-8356: Reworked Ibexa\Core\MVC\Symfony\Security\Authentication\AuthenticatorInterface usages to comply with Symfony-based authentication
    
    * fixed phpstan
    
    * improved code according to PHPStan reports
    
    * restored mutation and moved authorization elsewhere
    
    * fixed lack of proper password validation
    
    * cr remark

---
+++ src/lib/Mutation/AuthenticationMutation.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\GraphQL\Mutation;
+
+use Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException;
+use Ibexa\Contracts\Core\Repository\UserService;
+use Ibexa\Core\MVC\Symfony\Security\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Overblog\GraphQLBundle\Definition\Argument;
+
+final readonly class AuthenticationMutation
+{
+    public function __construct(
+        private JWTTokenManagerInterface $tokenManager,
+        private UserService $userService
+    ) {
+    }
+
+    /**
+     * @return array<string, ?string>
+     *
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException
+     */
+    public function createToken(Argument $args): array
+    {
+        if (!isset($args['username'], $args['password'])) {
+            return [
+                'message' => 'Missing username or password',
+                'token' => null,
+            ];
+        }
+
+        try {
+            $user = $this->userService->loadUserByLogin($args['username']);
+        } catch (NotFoundException) {
+            return $this->getWrongCredentialsErrorMessage();
+        }
+
+        if (!$this->userService->checkUserCredentials($user, $args['password'])) {
+            return $this->getWrongCredentialsErrorMessage();
+        }
+
+        return [
+            'token' => $this->tokenManager->create(new User($user)),
+        ];
+    }
+
+    /**
+     * @return array<string, ?string>
+     */
+    private function getWrongCredentialsErrorMessage(): array
+    {
+        return [
+            'message' => 'Wrong username or password',
+            'token' => null,
+        ];
+    }
+}
