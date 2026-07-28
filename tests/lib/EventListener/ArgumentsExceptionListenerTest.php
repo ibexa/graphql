ecf03e75dfa9c5f51016a928e48c5bda1cd226bb
Sebastian Jamuła <37102695+Sztig@users.noreply.github.com>
2026-07-28 15:02:58 +0200

    IBX-11507: Critical errors logged when browsing BackOffice as user with limited permissions (#100)
    * added new listener and log channel to handle ArgumentsException logging
    
    * refactored fix after review
    
    * implemented tests for the ArgumentsExceptionListener
    
    * fixed test for php7.4
    
    * added phpunit components to the bundle
    
    * refactored the test into phpunit
    
    * implemented suggested changes

---
+++ tests/lib/EventListener/ArgumentsExceptionListenerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\GraphQL\EventListener;
+
+use GraphQL\Error\Error;
+use Ibexa\GraphQL\DataLoader\Exception\ArgumentsException;
+use Ibexa\GraphQL\EventListener\ArgumentsExceptionListener;
+use Overblog\GraphQLBundle\Event\ErrorFormattingEvent;
+use Overblog\GraphQLBundle\Event\Events;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use RuntimeException;
+
+final class ArgumentsExceptionListenerTest extends TestCase
+{
+    /** @var \Psr\Log\LoggerInterface&\PHPUnit\Framework\MockObject\MockObject */
+    private LoggerInterface $logger;
+
+    private ArgumentsExceptionListener $listener;
+
+    protected function setUp(): void
+    {
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->listener = new ArgumentsExceptionListener($this->logger);
+    }
+
+    public function testGetSubscribedEvents(): void
+    {
+        self::assertSame(
+            [Events::ERROR_FORMATTING => ['onErrorFormatting', 10]],
+            ArgumentsExceptionListener::getSubscribedEvents(),
+        );
+    }
+
+    public function testOnErrorFormattingLogsAndStopsPropagationForArgumentsException(): void
+    {
+        $exception = new ArgumentsException('invalid arguments');
+        $event = $this->createErrorFormattingEvent($exception);
+
+        $this->logger
+            ->expects(self::once())
+            ->method('debug')
+            ->with(
+                sprintf('[GraphQL] %s: %s', ArgumentsException::class, 'invalid arguments'),
+                ['exception' => $exception],
+            );
+
+        $this->listener->onErrorFormatting($event);
+
+        self::assertTrue($event->isPropagationStopped());
+    }
+
+    public function testOnErrorFormattingIgnoresExceptionsThatAreNotArgumentsExceptions(): void
+    {
+        $exception = new RuntimeException('some other error');
+        $event = $this->createErrorFormattingEvent($exception);
+
+        $this->logger
+            ->expects(self::never())
+            ->method('debug');
+
+        $this->listener->onErrorFormatting($event);
+
+        self::assertFalse($event->isPropagationStopped());
+    }
+
+    private function createErrorFormattingEvent(\Throwable $exception): ErrorFormattingEvent
+    {
+        return new ErrorFormattingEvent(
+            new Error('Internal server error', null, null, [], null, $exception),
+            [],
+        );
+    }
+}
