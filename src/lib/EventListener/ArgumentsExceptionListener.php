33821364f3dcebdbe3b08878f5029d45c70066d1
Andrew Longosz <alongosz@users.noreply.github.com>
2026-09-13 19:59:02 +0200

    IBX-11778: Updated GitHub Actions workflows to org standard (#104)
    For more details see https://ibexa.atlassian.net/browse/IBX-11778 and https://github.com/ibexa/graphql/pull/104
    
    Key changes:
    
    * Renamed the CI workflow to "Backend CI" and added a bare workflow_dispatch trigger (IBX-12062)
    
    * Bumped actions/checkout to v7
    
    * Standardized all runner labels to ubuntu-26.04
    
    * Dropped the non-existent main branch from the push triggers of backend-ci
    
    * Passed the PHP version to the shared composer-install action explicitly (IBX-11907)
    
    * Guarded the code style check with set -euo pipefail so cs2pr no longer masks check-cs failures (IBX-11853)
    
    * Removed the deprecated pr-assign.yaml workflow
    
    * Bumped the ibexa/code-style dev requirement to ~1.3.0, the oldest maintained code-style line for 4.6 packages
    
    * Fixed code style in 120 files under src/ that needed a blank line after the file docblock (single_blank_line_before_namespace, enforced by ibexa/code-style 1.3)

--- src/lib/EventListener/ArgumentsExceptionListener.php
+++ src/lib/EventListener/ArgumentsExceptionListener.php
@@ -4,6 +4,7 @@
  * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+
 namespace Ibexa\GraphQL\EventListener;
 
 use Ibexa\GraphQL\DataLoader\Exception\ArgumentsException;
